924339cb889aaea16557a7b8a5dd3ae5af319eeb
Woojoung <chae3229@gmail.com>
2022-05-04 14:57:03 +0900

    update cfgs

--- tools/cfg.js
+++ tools/cfg.js
@@ -1,5 +1,6 @@
 'use strict'
 
-const env = 'localhost'
+const env = 'production'
 
-const apiUrl = 'http://127.0.0.1:7071/'
+const apiUrl = 'http://ec2-13-209-96-172.ap-northeast-2.compute.amazonaws.com:7071/'
+// const apiUrl = 'https://ec2-api.metaforest.us:7071/'
--- tools/cfg.production.js
+++ tools/cfg.production.js
@@ -3,4 +3,4 @@
 const env = 'production'
 
 const apiUrl = 'http://ec2-13-209-96-172.ap-northeast-2.compute.amazonaws.com:7071/'
-// const apiUrl = 'https://ec2-13-209-96-172.ap-northeast-2.compute.amazonaws.com:7071/'
+// const apiUrl = 'https://ec2-api.metaforest.us:7071/'
