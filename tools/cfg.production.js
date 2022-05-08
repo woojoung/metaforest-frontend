c4983de05d97acca574d66967d4f14e38fff7b35
woojoung <chae3229@gmail.com>
2022-05-09 02:30:54 +0900

    Update cfg.production.js

--- tools/cfg.production.js
+++ tools/cfg.production.js
@@ -1,6 +1,5 @@
 'use strict'
 
 const env = 'production'
-
-const apiUrl = 'http://ec2-13-209-96-172.ap-northeast-2.compute.amazonaws.com:7071/'
-// const apiUrl = 'https://ec2-13-209-96-172.ap-northeast-2.compute.amazonaws.com:7071/'
+const apiUrl = 'https://api.metaforest.us/'
+// const apiUrl = 'http://ec2-15-164-226-44.ap-northeast-2.compute.amazonaws.com:7071/'
