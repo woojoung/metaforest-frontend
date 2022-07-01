626911329ad81e18cb596f88ec5de95d271b4133
Woojoung <chae3229@gmail.com>
2022-07-01 11:20:54 +0900

    Update cfg.js

--- tools/cfg.js
+++ tools/cfg.js
@@ -3,4 +3,4 @@
 const env = 'production'
 
 // const apiUrl = 'http://ec2-15-164-226-44.ap-northeast-2.compute.amazonaws.com:7071/'
-const apiUrl = 'https://api.metaforest.us/'
+const apiUrl = 'http://127.0.0.1:7071/'
